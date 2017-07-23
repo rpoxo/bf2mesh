bf245cfe5076d6fbd843d8ea5eec0b92de05ef2c
rpoxo <irpoxotaujio@gmail.com>
2017-07-23 22:10:11 +0300

    added test for more complex real geometry

--- tests/tests_mesh_modify.py
+++ tests/tests_mesh_modify.py
@@ -7,7 +7,7 @@ import bf2
 import meshes
 import modmesh
 
-class TestStdMesOperations(unittest.TestCase):
+class TestStdMeshOperations(unittest.TestCase):
 
     def setUp(self):
         box_std = os.path.join(*['objects', 'staticobjects', 'test', 'evil_box', 'meshes', 'evil_box.staticmesh'])
@@ -21,7 +21,7 @@ class TestStdMesOperations(unittest.TestCase):
     def test_can_offset_mesh(self):
         vmesh = meshes.LoadBF2Mesh(self.box_std)
 
-        offset = (0.0, 0.0, 10.0)
+        offset = (0.0, 0.0, 1.0)
         modmesh.offset_vertices(vmesh, offset)
         vmesh.write_file_data(self.modified_box_position)
     
@@ -32,11 +32,22 @@ class TestStdMesOperations(unittest.TestCase):
         modmesh.offset_UV1(vmesh, offset)
         vmesh.write_file_data(self.modified_box_UV1)
 
+class TestMaterialsOptimization(unittest.TestCase):
 
+    def setUp(self):
+        # File: "D:\Games\Project Reality\mods\pr_repo\objects\weapons\Handheld\GBRIF_L85A2iron\meshes\gbrif_l85a2iron.bundledmesh"
+        l85_mesh = os.path.join(*['objects', 'weapons', 'Handheld', 'GBRIF_L85A2iron', 'meshes', 'gbrif_l85a2iron.bundledmesh'])
+        modified_mesh_position = os.path.join(*['objects', 'weapons', 'Handheld', 'GBRIF_L85A2iron', 'meshes', 'gbrif_l85a2iron_position.bundledmesh'])
+        
+        self.l85_mesh = os.path.join(bf2.Mod().root, l85_mesh)
+        self.modified_mesh_position = os.path.join(bf2.Mod().root, modified_mesh_position)
 
+    @unittest.skip('disabled: only for manual use')
+    def test_can_offset_mesh(self):
+        vmesh = meshes.LoadBF2Mesh(self.l85_mesh)
 
-
-
-
+        offset = (0.0, 0.0, 1.0)
+        modmesh.offset_vertices(vmesh, offset)
+        vmesh.write_file_data(self.modified_mesh_position)
 
 
